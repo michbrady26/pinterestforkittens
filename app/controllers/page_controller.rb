3485456bf9340207558a33cfd00381d1ead39cef
michbrady26 <mbrady4@bates.edu>
2014-05-07 16:06:59 -0400

    sleeping kittens page

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,4 +1,7 @@
 class PageController < ApplicationController
   def about
   end
+  
+  def sleepingkittens
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Pinterestforkittens::Application.routes.draw do
   get "/about" => "page#about"
   root "page#index"
   
+  get "/sleepingkittens" => "page#sleepingkittens"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
